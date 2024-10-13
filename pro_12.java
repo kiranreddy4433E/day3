15189eb16a911f04d8b19951207b44c7cfcc8139
Chandra Kiran Reddy Reddycharla <75166642+kiranreddy4433E@users.noreply.github.com>
2024-10-13 16:33:52 +0530

    Add files via upload

---
+++ pro_12.java
@@ -0,0 +1,18 @@
+package dsa;
+import java.util.Scanner;
+public class pro_12 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter a  character");
+		char ch = input.next().charAt(0);
+		System.out.println("you entered character :- " + ch);
+		int asciiValue = ch ;
+		System.out.println("The ASCII value of " + ch + " is = " + asciiValue);
+				
+		
+		
+	}
+
+}
+
